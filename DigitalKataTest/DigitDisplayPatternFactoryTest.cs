2586df56f63023e1b8d8cbd9a0056f581a6fee66
Joshua Carmody <contact@joshuacarmody.com>
2015-02-25 20:15:26 -0500

    Added tests for converting zero and extreme values of int to digital representations

--- DigitalKataTest/DigitDisplayPatternFactoryTest.cs
+++ DigitalKataTest/DigitDisplayPatternFactoryTest.cs
@@ -56,7 +56,7 @@ namespace DigitalKataTest
         }
 
         [TestMethod]
-        public void CreateEnumerableFrom_Should_Return_Single_Item_List_Given_Single_Digit_Int()
+        public void CreateEnumerable_Should_Return_Single_Item_List_Given_Single_Digit_Int()
         {
             IEnumerable<DigitDisplayPattern> digits = DigitDisplayPatternFactory.CreateEnumerable(5);
 
@@ -64,7 +64,16 @@ namespace DigitalKataTest
         }
 
         [TestMethod]
-        public void CreateEnumerableFrom_Should_Return_OneZero_Given_10()
+        public void CreateEnumerable_Should_Return_Zero_Given_Zero()
+        {
+            IEnumerable<DigitDisplayPattern> digits = DigitDisplayPatternFactory.CreateEnumerable(0);
+
+            Assert.AreEqual(1, digits.Count());
+            Assert.AreEqual(DigitDisplayPatternFactory.Zero, digits.First());
+        }
+
+        [TestMethod]
+        public void CreateEnumerable_Should_Return_OneZero_Given_10()
         {
             var digits = DigitDisplayPatternFactory.CreateEnumerable(10).ToArray();
 
@@ -73,12 +82,38 @@ namespace DigitalKataTest
         }
 
         [TestMethod]
-        public void CreateEnumerableFrom_Should_Return_MinusOne_Given_Minus1()
+        public void CreateEnumerable_Should_Return_MinusOne_Given_Minus1()
         {
             var digits = DigitDisplayPatternFactory.CreateEnumerable(-1).ToArray();
 
             Assert.AreEqual(DigitDisplayPatternFactory.Minus, digits[0]);
             Assert.AreEqual(DigitDisplayPatternFactory.One, digits[1]);
+        }
+
+        [TestMethod]
+        public void CreateEnumerable_Should_Return_ThreeTwoSevenSixSeven_Given_IntMax()
+        {
+            var digits = DigitDisplayPatternFactory.CreateEnumerable(Int16.MaxValue).ToArray();
+
+            Assert.AreEqual(DigitDisplayPatternFactory.Three, digits[0]);
+            Assert.AreEqual(DigitDisplayPatternFactory.Two, digits[1]);
+            Assert.AreEqual(DigitDisplayPatternFactory.Seven, digits[2]);
+            Assert.AreEqual(DigitDisplayPatternFactory.Six, digits[3]);
+            Assert.AreEqual(DigitDisplayPatternFactory.Seven, digits[4]);
+        }
+
+
+        [TestMethod]
+        public void CreateEnumerable_Should_Return_MinusThreeTwoSevenSixEight_Given_IntMin()
+        {
+            var digits = DigitDisplayPatternFactory.CreateEnumerable(Int16.MinValue).ToArray();
+
+            Assert.AreEqual(DigitDisplayPatternFactory.Minus, digits[0]);
+            Assert.AreEqual(DigitDisplayPatternFactory.Three, digits[1]);
+            Assert.AreEqual(DigitDisplayPatternFactory.Two, digits[2]);
+            Assert.AreEqual(DigitDisplayPatternFactory.Seven, digits[3]);
+            Assert.AreEqual(DigitDisplayPatternFactory.Six, digits[4]);
+            Assert.AreEqual(DigitDisplayPatternFactory.Eight, digits[5]);
         }
     }
 }
